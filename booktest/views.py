feb878ed028bb8d9e82c7bd043d5e82ebb85a145
ehananel-lirf23 <jilun82415819@126.com>
2021-04-12 16:55:24 +0800

    获取所有图书接口

--- booktest/views.py
+++ booktest/views.py
@@ -8,13 +8,28 @@ DELETE  /books/<pk>/    删除指定id的记录
 响应数据    JSON
 """
 from django.views.generic import View
+from .models import BookInfo
+from django.http.response import JsonResponse, HttpResponse
 
 
 class BooksAPIView(View):
     """获取所有和新增"""
 
     def get(self, request):
-        pass
+        """查询所有图书"""
+        # 1.查询所有图书模型数据
+        books = BookInfo.objects.all()
+        book_dict_list = []
+        for book in books:
+            book_dict = {
+                'id': book.id,
+                'btitle': book.btitle,
+                'bpub_date': book.bpub_date,
+                'bread': book.bread,
+                'bcomment': book.bcomment,
+            }
+            book_dict_list.append(book_dict)
+        return JsonResponse(book_dict_list, safe=False)  # safe 默认是True 设置safe=False，不安全，因为传列表里面有字典。
 
     def post(self, request):
         pass
