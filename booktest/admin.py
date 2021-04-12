3a6604b56cebb184a79c5ecb7701d3b109193754
ehananel-lirf23 <jilun82415819@126.com>
2021-04-12 15:25:05 +0800

    app应用配置 & 注册模型

--- booktest/admin.py
+++ booktest/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import BookInfo, HeroInfo
+
+# 注册模型 在后台 可以操作
+admin.site.register(BookInfo)  # 在admin站点中注册模型
+admin.site.register(HeroInfo)
--- booktest/apps.py
+++ booktest/apps.py
@@ -2,4 +2,6 @@ from django.apps import AppConfig
 
 
 class BooktestConfig(AppConfig):
-    name = 'booktest'
+    """配置 子应用 信息"""
+    name = 'booktest'  # 加载子应用时,name表示所加载的是那个名字的应用,尽量不要改
+    verbose_name = "图书管理系统"  # admin站点上显示子应用的名字
